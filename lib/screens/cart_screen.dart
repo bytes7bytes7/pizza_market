2c72ea6dcf8aa095b8de073b8f251ba6ca11be79
bytes7bytes7 <bytes7bytes7@gmail.com>
2022-07-19 21:00:29 +0300

    create PizzaControlCard

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../blocs/blocs.dart';
 import '../l10n/l10n.dart';
 import '../widgets/widgets.dart';
 
@@ -11,13 +13,63 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+  late final PizzaBloc _pizzaBloc;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pizzaBloc = context.read<PizzaBloc>();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     final l10n = context.l10n;
 
     return Scaffold(
       appBar: DefaultAppBar(
         title: l10n.orderDetails,
+      ),
+      body: BlocBuilder<PizzaBloc, PizzaState>(
+        builder: (context, state) {
+          if (state is PizzaInitState) {
+            _pizzaBloc.add(const PizzaLoadEvent());
+          }
+
+          if (state is PizzaDataState) {
+            final inCart = state.wrappers.where((e) => e.amount > 0).toList();
+
+            if (inCart.isEmpty) {
+              return Center(
+                child: Text(
+                  l10n.empty,
+                  style: theme.textTheme.headline6,
+                ),
+              );
+            }
+
+            return ListView.builder(
+              itemCount: inCart.length,
+              itemBuilder: (context, index) {
+                return PizzaControlCard(
+                  wrapper: inCart[index],
+                );
+              },
+            );
+          } else if (state is PizzaErrorState) {
+            return ErrorLabel(
+              error: state.error,
+              stackTrace: state.stackTrace,
+              buttonText: l10n.tryAgain,
+              onPressed: () {
+                _pizzaBloc.add(const PizzaLoadEvent());
+              },
+            );
+          }
+
+          return const LoadingWidget();
+        },
       ),
     );
   }
---
+++ lib/widgets/pizza_control_card.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:pizza_repo/pizza_repo.dart';
+
+import '../constants/measures.dart' as const_measures;
+import 'amount_bar.dart';
+import 'common.dart';
+
+class PizzaControlCard extends StatefulWidget {
+  const PizzaControlCard({
+    super.key,
+    required this.wrapper,
+  });
+
+  final PizzaWrapper wrapper;
+
+  @override
+  State<PizzaControlCard> createState() => _PizzaControlCardState();
+}
+
+class _PizzaControlCardState extends State<PizzaControlCard> {
+  late final ValueNotifier<int> _amountNotifier;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _amountNotifier = ValueNotifier(widget.wrapper.amount);
+  }
+
+  @override
+  void dispose() {
+    _amountNotifier.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: const_measures.bigPadding,
+        vertical: const_measures.midPadding,
+      ),
+      padding: const EdgeInsets.all(
+        const_measures.smallPadding,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          const_measures.midRadius,
+        ),
+        color: theme.scaffoldBackgroundColor,
+        boxShadow: [
+          BoxShadow(
+            color: theme.shadowColor,
+            offset: const_measures.shadowOffset,
+            blurRadius: const_measures.blurRadius,
+          ),
+        ],
+      ),
+      child: IntrinsicHeight(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: const_measures.midImageSize,
+              width: const_measures.midImageSize,
+              child: (widget.wrapper.imageUrl.isNotEmpty)
+                  ? Image.network(
+                      widget.wrapper.imageUrl,
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, trace) {
+                        return const Icon(Icons.photo);
+                      },
+                    )
+                  : const Icon(Icons.local_pizza_outlined),
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: const_measures.bigPadding,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        widget.wrapper.title,
+                        maxLines: 2,
+                        style: theme.textTheme.headline6,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(
+                        '\$${beautifyCost(widget.wrapper.price)}',
+                        style: theme.textTheme.headline6?.copyWith(
+                          color: theme.primaryColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            AmountBar(
+              notifier: _amountNotifier,
+              max: widget.wrapper.maxAmount,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
